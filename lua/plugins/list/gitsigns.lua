46b9705f35176016da9bb4e9f3ea3d3c3de14d78
João Fraga <joao@fraguinha.com>
2023-07-15 18:30:53 +0100

    Update configurations

--- lua/plugins/list/gitsigns.lua
+++ lua/plugins/list/gitsigns.lua
@@ -27,11 +27,6 @@ return {
                     return "<Ignore>"
                 end, { buffer = bufnr })
 
-                vim.keymap.set("n", "<leader>hs", gs.stage_hunk)
-                vim.keymap.set("n", "<leader>hS", gs.stage_buffer)
-                vim.keymap.set("n", "<leader>hu", gs.undo_stage_hunk)
-                vim.keymap.set("n", "<leader>hr", gs.reset_hunk)
-                vim.keymap.set("n", "<leader>hR", gs.reset_buffer)
                 vim.keymap.set("n", "<leader>hh", gs.preview_hunk)
                 vim.keymap.set("n", "<leader>hb", function() gs.blame_line { full = true } end)
                 vim.keymap.set("n", "<leader>hd", gs.toggle_deleted)
