2c7efd318157ec95a79cd06af2f9d8ffc717e2cf
Joao Fraga <joao.fraga@feedzai.com>
2023-07-26 10:08:46 +0100

    Update configurations

--- lua/plugins/list/navigator.lua
+++ lua/plugins/list/navigator.lua
@@ -4,9 +4,9 @@ return {
         local nav = require("Navigator")
         nav.setup()
 
-        vim.keymap.set("n", "<C-h>", nav.left)
-        vim.keymap.set("n", "<C-j>", nav.down)
-        vim.keymap.set("n", "<C-k>", nav.up)
-        vim.keymap.set("n", "<C-l>", nav.right)
+        vim.keymap.set({ "n", "i" }, "<C-h>", nav.left)
+        vim.keymap.set({ "n", "i" }, "<C-j>", nav.down)
+        vim.keymap.set({ "n", "i" }, "<C-k>", nav.up)
+        vim.keymap.set({ "n", "i" }, "<C-l>", nav.right)
     end
 }
