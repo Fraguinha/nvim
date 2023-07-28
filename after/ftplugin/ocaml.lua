4f0f3d7e1309a08fc120465e48367026d0269f00
João Fraga <joao@fraguinha.com>
2023-07-28 01:00:11 +0100

    Update configurations

--- after/ftplugin/ocaml.lua
+++ after/ftplugin/ocaml.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     group = Format_group,
     pattern = "*",
-    callback = function(ev)
+    callback = function(_)
         vim.lsp.buf.format()
     end,
 })
