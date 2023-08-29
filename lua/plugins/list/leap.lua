f38979a04ebbb561b909d5352707d0ebd70298fd
Joao Fraga <joao.fraga@feedzai.com>
2023-08-29 16:58:12 +0100

    Update configurations

--- lua/plugins/list/leap.lua
+++ lua/plugins/list/leap.lua
@@ -8,18 +8,6 @@ return {
 
         vim.api.nvim_set_hl(0, "LeapLabelPrimary", { fg = "red", bg = "none" })
 
-        vim.keymap.set("n", "s", function()
-            local current_window = vim.fn.win_getid()
-            leap.leap { target_windows = { current_window } }
-        end)
-        vim.keymap.set("o", "x", function()
-            local current_window = vim.fn.win_getid()
-            leap.leap { target_windows = { current_window } }
-        end)
-
-        leap.add_repeat_mappings(';', ',', {
-            relative_directions = false,
-            modes = { 'n', 'x', 'o' },
-        })
+        leap.add_default_mappings()
     end,
 }
