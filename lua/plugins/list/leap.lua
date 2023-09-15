f4a0b1af189dbbb64fc9b57815c1d2cfe436b441
Joao Fraga <joao.fraga@feedzai.com>
2023-09-15 17:03:50 +0100

    Update configurations

--- lua/plugins/list/leap.lua
+++ lua/plugins/list/leap.lua
@@ -9,5 +9,10 @@ return {
         vim.api.nvim_set_hl(0, "LeapLabelPrimary", { fg = "red", bg = "none" })
 
         leap.add_default_mappings()
+
+        vim.keymap.set("n", "s", function()
+            local current_window = vim.fn.win_getid()
+            require('leap').leap { target_windows = { current_window } }
+        end)
     end,
 }
--- lua/plugins/list/surround.lua
+++ lua/plugins/list/surround.lua
@@ -1,6 +1,19 @@
 return {
     "kylechui/nvim-surround",
     config = function()
-        require("nvim-surround").setup()
+        require("nvim-surround").setup(
+            {
+                keymaps = {
+                    normal = "S",
+                    normal_cur = "SS",
+                    normal_line = "gS",
+                    normal_cur_line = "gSS",
+                    visual = "S",
+                    visual_line = "S",
+                    delete = "dS",
+                    change = "cS",
+                },
+            }
+        )
     end
 }
