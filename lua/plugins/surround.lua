Fraguinha/vim

feat: Migrate neovim configuration

lua/plugins/surround.lua
@@ -1,0 +1,14 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "s",
+      delete = "ds",
+      find = nil,
+      find_left = nil,
+      highlight = nil,
+      replace = "cs",
+      update_n_lines = nil,
+    },
+  },
+}

Fraguinha/nvim

lua/plugins/surround.lua
@@ -1,0 +1,14 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "ys",
+      delete = "ds",
+      find = nil,
+      find_left = nil,
+      highlight = nil,
+      replace = "cs",
+      update_n_lines = nil,
+    },
+  },
+}